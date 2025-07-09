85a7115f7458b6cf5f978654ae0e8fb2448ef650
Daniel-Capla <capla.daniel@gmail.com>
2025-07-09 18:28:21 +0200

    Add user entity, Set up docke and db

---
+++ src/main/kotlin/com/morosys/userscrud/models/User.kt
@@ -0,0 +1,19 @@
+package com.morosys.userscrud.models
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import java.util.UUID
+
+@Entity
+class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: UUID? = null,
+    val firstName: String,
+    val lastName: String,
+    val userName: String? = null,
+    val email: String,
+    val password: String
+)
